4b5db73a17921a3253a1877467ada3d4f0b2848b
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2019-12-17 00:00:10 +0530

    api
    update

---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url : "mongodb://test:test@cluster0-shard-00-00-0c64g.mongodb.net:27017,cluster0-shard-00-01-0c64g.mongodb.net:27017,cluster0-shard-00-02-0c64g.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
+}
